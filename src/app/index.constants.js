bb1059d21bf6eb02b6133c6b4654f1a127a150e3
Ramon Boss <ramon.boss@outlook.com>
2015-12-21 12:13:45 +0100

    added dummy name

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -5,6 +5,6 @@
   angular
     .module('familyTreeFrontend')
     .constant('apiHost', 'http://localhost:3000/api')
-    .constant('rootMember', { firstName: 'Hans', lastName: 'Ramser' });
+    .constant('rootMember', { firstName: 'Max', lastName: 'Muster' });
 
 })();
