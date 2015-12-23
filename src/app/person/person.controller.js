42abe9791dc340f04c953db74f5f14e95b9b3f88
Ramon Boss <ramon.boss@outlook.com>
2015-12-23 20:43:17 +0100

    first look

--- src/app/person/person.controller.js
+++ src/app/person/person.controller.js
@@ -6,7 +6,40 @@
     .controller('PersonController', PersonController);
 
   /** @ngInject */
-  function PersonController() {
+  function PersonController($scope, $stateParams, familyMemberRepository, imageRepository) {
+    $scope.title = '';
+    $scope.imgSrc = '';
+    $scope.info = {};
+
+
+    familyMemberRepository
+      .query($stateParams.id)
+      .then(function(response) {
+        setInfo(response.id);
+        setTitle(response);
+        setImage(response);
+        setAge(response);
+      });
+
+    function setTitle(person) {
+      $scope.title = familyMemberRepository.getNamesAsString(person);
+    }
+
+    function setImage(person) {
+      $scope.imgSrc = imageRepository.getImageName(person);
+    }
+
+    function setAge(person) {
+      $scope.info.age = familyMemberRepository.getAge(person);
+    }
+
+    function setInfo(id) {
+      familyMemberRepository
+        .getInfo(id)
+        .then(function(response) {
+          $scope.info = angular.extend($scope.info, response);
+        });
+    }
 
   }
 
