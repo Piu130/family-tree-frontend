bde40d15ca6d46214433807542b428267f064871
Ramon Boss <ramon.boss@outlook.com>
2016-01-04 09:27:14 +0100

    first test for familyMember object

--- src/app/components/dataRessource/familyMember.factory.js
+++ src/app/components/dataRessource/familyMember.factory.js
@@ -1,3 +1,31 @@
-/**
- * Created by piu130 on 23.12.15.
- */
+(function() {
+  'use strict';
+
+  angular
+    .module('familyTreeFrontend')
+    .factory('FamilyMember', familyMember);
+
+  /** @ngInject */
+  function familyMember() {
+    function constructor(familyMember) {
+      //return FamilyMember[] = familyMember;
+    }
+
+    function FamilyMember() {
+
+    }
+
+    FamilyMember.prototype.getAge = function() {
+      var today = new Date();
+      var birthDay = new Date(this.birthDay);
+      var age = today.getFullYear() - birthDay.getFullYear();
+      var m = today.getMonth() - birthDay.getMonth();
+      if (m < 0 || (m === 0 && today.getDate() < birthDay.getDate())) {
+        age--;
+      }
+      return age;
+    };
+
+    return constructor;
+  }
+})();
