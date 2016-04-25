06ade9e5d4254586c928234a43c1487f500d0592
Ramon Boss <ramon.boss@outlook.com>
2016-04-25 09:49:53 +0200

    replaced old gulp-minify-css with new gulp-clean-css

--- gulp/build.js
+++ gulp/build.js
@@ -51,7 +51,7 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe(cssFilter)
     .pipe($.sourcemaps.init())
     .pipe($.replace('../../bower_components/bootstrap-sass/assets/fonts/bootstrap/', '../fonts/'))
-    .pipe($.minifyCss({ processImport: false }))
+    .pipe($.cleanCss({ processImport: false }))
     .pipe($.sourcemaps.write('maps'))
     .pipe(cssFilter.restore)
     .pipe(assets.restore())
