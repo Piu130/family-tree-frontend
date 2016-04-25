186c3905104278f49c5d152104d9643c337ce27d
Ramon Boss <ramon.boss@outlook.com>
2016-04-25 10:22:44 +0200

    removed wrench from devDependencies

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,23 @@
 
 'use strict';
 
+var fs = require('fs');
 var gulp = require('gulp');
-var wrench = require('wrench');
 
 /**
  *  This will load all js or coffee files in the gulp directory
  *  in order to load all gulp tasks
  */
-wrench.readdirSyncRecursive('./gulp').filter(function (file) {
-  return (/\.(js|coffee)$/i).test(file);
-}).map(function (file) {
-  require('./gulp/' + file);
-});
+var items = [];
+
+fs
+  .readdirSync('./gulp')
+  .filter(function (file) {
+    return (/\.(js|coffee)$/i).test(file);
+  })
+  .map(function (file) {
+    require('./gulp/' + file);
+  });
 
 
 /**
