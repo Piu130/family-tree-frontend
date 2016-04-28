1ea68eaa02081b50faeb7cf40ec0b3a48bdf7873
Ramon Boss <ramon.boss@outlook.com>
2016-04-28 10:24:20 +0200

    bumped gulp-useref

--- gulp/build.js
+++ gulp/build.js
@@ -36,12 +36,10 @@ gulp.task('html', ['inject', 'partials'], function () {
   var htmlFilter = $.filter('*.html', { restore: true });
   var jsFilter = $.filter('**/*.js', { restore: true });
   var cssFilter = $.filter('**/*.css', { restore: true });
-  var assets;
 
   return gulp.src(path.join(conf.paths.tmp, '/serve/*.html'))
     .pipe($.inject(partialsInjectFile, partialsInjectOptions))
-    .pipe(assets = $.useref.assets())
-    .pipe($.rev())
+    .pipe($.useref())
     .pipe(jsFilter)
     .pipe($.sourcemaps.init())
     .pipe($.ngAnnotate())
@@ -54,8 +52,6 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe($.cleanCss({ processImport: false }))
     .pipe($.sourcemaps.write('maps'))
     .pipe(cssFilter.restore)
-    .pipe(assets.restore())
-    .pipe($.useref())
     .pipe($.revReplace())
     .pipe(htmlFilter)
     .pipe($.minifyHtml({
