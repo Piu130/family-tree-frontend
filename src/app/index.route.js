3eefe4dcb473572d1944b954a3ac807bd0cb7882
Ramon Boss <ramon.boss@outlook.com>
2016-04-29 10:41:46 +0200

    added authentication for routes

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -12,34 +12,39 @@
         url: '/login',
         templateUrl: 'app/login/login.html',
         controller: 'LoginController',
-        controllerAs: 'login'
+        controllerAs: 'login',
+        authenticate: false
       })
       .state('tree', {
         url: '/',
         templateUrl: 'app/tree/tree.html',
         controller: 'TreeController',
-        controllerAs: 'tree'
+        controllerAs: 'tree',
+        authenticate: false
       })
       .state('person', {
         url: '/person/:id',
         templateUrl: 'app/person/person.html',
         controller: 'PersonController',
-        controllerAs: 'person'
+        controllerAs: 'person',
+        authenticate: true
       })
       .state('emblem', {
         url: '/emblem',
         templateUrl: 'app/emblem/emblem.html',
         controller: 'EmblemController',
-        controllerAs: 'emblem'
+        controllerAs: 'emblem',
+        authenticate: false
       })
       .state('admin', {
         url: '/admin',
         templateUrl: 'app/admin/admin.html',
         controller: 'AdminController',
-        controllerAs: 'admin'
+        controllerAs: 'admin',
+        authenticate: true
       });
 
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/login');
   }
 
 })();
--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,11 +6,27 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock($rootScope, $log, $state, $translate, growl, loginService) {
 
     String.prototype.removeLast = function () {
       return this.substring(0, this.length - 1);
     };
+
+    $rootScope.$on('$stateChangeStart', function(event, toState){
+
+      // Move this to a service
+      if (toState.authenticate && !loginService.isAuthenticated()){
+        // User isn’t authenticated
+        growl.error($translate.instant('NOT_AUTHENTICATED'), {
+          ttl: 3000,
+          disableCountDown: true,
+          disableIcons: true
+        });
+
+        $state.transitionTo('login');
+        event.preventDefault();
+      }
+    });
 
     $log.debug('runBlock end');
   }
