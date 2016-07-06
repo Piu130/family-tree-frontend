0c3c575500a148b923c14fdf35b9268188aabdb5
Ramon Boss <ramon.boss@outlook.com>
2016-07-06 11:07:52 +0200

    seperated schema for better storing

--- src/app/admin/admin.controller.js
+++ src/app/admin/admin.controller.js
@@ -13,17 +13,9 @@
     const vm = this;
 
     vm.dropdownSpouseParent = [];
-    vm.form = [
-      '*',
-      {
-        type: 'submit',
-        title: 'Save'
-      }
-    ];
-    vm.image = {};
-    vm.model = {};
-    vm.parent = '';
-    vm.schema = {
+    vm.allFamilyMembers = [];
+    vm.currentPerson = {};
+    vm.schemaMain = {
       type: 'object',
       properties: {
         firstNames: {
@@ -38,63 +30,93 @@
             type: 'string'
           }
         },
-        gender: {
-          type: 'string',
-          enum: ['male', 'female', 'transgender', 'other']
-        },
         dayOfDeath: {
           type: 'string',
           format: 'date'
         },
         order: {
           type: 'number'
-        },
-        info: {
-          type: 'object',
-          properties: {
-            birthday: {
-              type: 'string',
-              format: 'date'
-            },
-            size: {
-              type: 'number'
-            },
-            apprenticeships: {
-              type: 'array',
-              items: {
-                type: 'string'
-              }
-            },
-            studies: {
-              type: 'array',
-              items: {
-                type: 'string'
-              }
-            },
-            professions: {
-              type: 'array',
-              items: {
-                type: 'string'
-              }
-            },
-            hobbies: {
-              type: 'array',
-              items: {
-                type: 'string'
-              }
-            },
-            favouriteColor: {
-              type: 'string'
-            },
-            childhoodDream: {
-              type: 'string'
-            }
-          }
         }
       }
     };
+    vm.schemaInfo = {
+      type: 'object',
+      properties: {
+        gender: {
+          type: 'string',
+          enum: ['male', 'female', 'transgender', 'other']
+        },
+        birthday: {
+          type: 'string',
+          format: 'date'
+        },
+        size: {
+          type: 'number'
+        },
+        apprenticeships: {
+          type: 'array',
+          items: {
+            type: 'string'
+          }
+        },
+        studies: {
+          type: 'array',
+          items: {
+            type: 'string'
+          }
+        },
+        professions: {
+          type: 'array',
+          items: {
+            type: 'string'
+          }
+        },
+        hobbies: {
+          type: 'array',
+          items: {
+            type: 'string'
+          }
+        },
+        favouriteColor: {
+          type: 'string'
+        },
+        childhoodDream: {
+          type: 'string'
+        }
+      }
+    };
+    vm.formMain = [
+      '*',
+      {
+        type: 'submit',
+        title: 'Save'
+      }
+    ];
+    vm.formInfo = [
+      '*',
+      {
+        type: 'submit',
+        title: 'Save'
+      }
+    ];
+    vm.modelMain = {};
+    vm.modelInfo = {};
+
+    vm.form = [
+      '*',
+      {
+        type: 'submit',
+        title: 'Save'
+      }
+    ];
+    vm.image = null;
+    vm.model = {};
+    vm.parent = '';
+    vm.editChange = editChange;
     vm.spouse = '';
     vm.submit = submit;
+    vm.submitMain = submitMain;
+    vm.submitInfo = submitInfo;
 
     activate();
 
@@ -102,16 +124,57 @@
       familyMemberRepository
         .get()
         .then(function (response) {
+          vm.allFamilyMembers = response
+            .map(function (familyMember) {
+              return {id: familyMember.id, label: familyMemberRepository.getNamesAsString(familyMember)}
+            });
           vm.dropdownSpouseParent = response
-            .filter(function (value) {
-              return !value.spouseId;
+            .filter(function (familyMember) {
+              return !familyMember.spouseId;
             })
-            .map(function (element) {
-              return {id: element.id, label: familyMemberRepository.getNamesAsString(element)}
+            .map(function (familyMember) {
+              return {id: familyMember.id, label: familyMemberRepository.getNamesAsString(familyMember)}
             });
 
           return response;
         });
+    }
+
+    function editChange() {
+      familyMemberRepository
+        .query(vm.currentPerson.id)
+        .then(function(response) {
+          vm.modelMain = response;
+        });
+
+      familyMemberRepository
+        .getInfo(vm.currentPerson.id)
+        .then(function(response) {
+          vm.modelInfo = response;
+        });
+      console.log(vm.currentPerson);
+    }
+
+    function submitMain() {
+      if (vm.spouse.id) {
+        vm.modelMain.spouseId = vm.spouse.id
+      }
+      if (vm.parent.id) {
+        vm.modelMain.parentId = vm.parent.id
+      }
+
+      familyMemberRepository
+        .put(vm.modelMain);
+
+      if(vm.image) {
+        imageRepository
+          .uploadProfilePicture(vm.image, vm.currentPerson);
+      }
+    }
+
+    function submitInfo() {
+      familyMemberRepository
+        .setInfo(vm.currentPerson.id, vm.modelInfo);
     }
 
     function submit() {
