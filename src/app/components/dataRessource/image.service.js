40572eb54a1451f6fe46093e526a8715a742b6d6
Ramon Boss <ramon.boss@outlook.com>
2016-07-06 09:52:01 +0200

    changes path and added acces_token to url

--- src/app/components/dataRessource/image.service.js
+++ src/app/components/dataRessource/image.service.js
@@ -6,7 +6,7 @@
     .factory('imageRepository', imageRepository);
 
   /** @ngInject */
-  function imageRepository($http, apiHost, Upload) {
+  function imageRepository($http, apiHost, Upload, $cookies) {
 
     return {
       getImgSrc: getImgSrc,
@@ -16,18 +16,18 @@
     };
 
     function getImgSrc(person) {
-      return apiHost + '/containers/familyMemberImages/download/' + person.id + '_profilepicture.jpg';
+      return apiHost + '/containers/family_member_images/download/' + person.id + '_profilepicture.jpg?access_token=' + $cookies.get('family_tree_access_token');
     }
 
     function download(file) {
       return $http
-        .get(apiHost + '/containers/familyMemberImages/download/' + file);
+        .get(apiHost + '/containers/family_member_images/download/' + file);
     }
 
     function upload(file, fileName) {
 
       return Upload.upload({
-        url: apiHost + '/containers/familyMemberImages/upload',
+        url: apiHost + '/containers/family_member_images/upload',
         data: { file: Upload.rename(file, fileName) }
       });
     }
